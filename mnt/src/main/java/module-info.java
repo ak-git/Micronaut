4fd2eaa56d85f89e28448ee551d91796c3fc0a73
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-09-29 12:37:47 +0300

    Remove Mockito and logback-classic dependencies

--- mnt/src/main/java/module-info.java
+++ mnt/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.ak.micronaut {
   requires org.jspecify;
   requires org.slf4j;
   requires org.yaml.snakeyaml;
-  requires ch.qos.logback.core;
-  requires ch.qos.logback.classic;
   requires jakarta.inject;
   requires jakarta.annotation;
   requires io.micronaut.context;
